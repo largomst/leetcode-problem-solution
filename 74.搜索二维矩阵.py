75a4055acf2bccb661123cb05b05054ec7ab912c
largomst <dingrui.zhen@gmail.com>
2021-09-24 15:31:43 +0800

    add 74

--- 74.搜索二维矩阵.py
+++ 74.搜索二维矩阵.py
@@ -25,10 +25,22 @@ def binarySearch(nums: List[int], target) -> int:
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for row in matrix:
-            if row[0] <= target <= row[-1]:
-                if binarySearch(row, target) != -1:
-                    return True
-        return False
+        l = 0
+        r = len(matrix) - 1
+        while l <= r:
+            mid = (l+r)//2
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                return binarySearch(matrix[mid], target) != -1
+            elif matrix[mid][0] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
 
+        return False
 # @lc code=end
+
+
+r = Solution().searchMatrix(
+    [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3)
+r = Solution().searchMatrix([[1]], 0)
+print(r)
