57d1240cb8c8b84ee17c3c7e1683038f281f0b3a
largomst <dingrui.zhen@gmail.com>
2021-09-26 10:26:07 +0800

    add debug comment

--- 206.反转链表.4.py
+++ 206.反转链表.4.py
@@ -4,6 +4,14 @@
 # [206] 反转链表
 #
 
+count = 0
+
+
+def printIndent(n):
+    for _ in range(n):
+        print(' ', end='')
+
+
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
@@ -14,11 +22,20 @@
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
+        # global count
+        # printIndent(count)
+        # print(f'args: {head.val}')
         if not head or not head.next:
+            # printIndent(count)
+            # print(f'return {head.val}')
             return head
+        # count += 1
         last = self.reverseList(head.next)
+        # count -= 1
         head.next.next = head
         head.next = None
+        # printIndent(count)
+        # print(f'return {head.val}')
         return last
 
 
