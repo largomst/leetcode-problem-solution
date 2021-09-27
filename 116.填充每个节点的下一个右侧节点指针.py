900e17acdf2932039ce8b1e887273d6f431d11d6
largomst <dingrui.zhen@gmail.com>
2021-09-27 16:06:08 +0800

    add 116

---
+++ 116.填充每个节点的下一个右侧节点指针.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode.cn id=116 lang=python3
+#
+# [116] 填充每个节点的下一个右侧节点指针
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        def connectTwoNode(node1, node2):
+            if not node1 or not node2:
+                return
+
+            node1.next = node2
+            connectTwoNode(node1.left, node1.right)
+            connectTwoNode(node2.left, node2.right)
+            connectTwoNode(node1.right, node2.left)
+
+        if not root:
+            return root
+
+        connectTwoNode(root.left, root.right)
+
+        return root
+
+# @lc code=end
