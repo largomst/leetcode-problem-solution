26bdc30a37ed23aa9144443e5ef87082a891b0ed
largomst <dingrui.zhen@gmail.com>
2021-10-09 18:22:02 +0800

    add 1143 dp

---
+++ 1143.最长公共子序列.2.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=1143 lang=python3
+#
+# [1143] 最长公共子序列
+#
+
+# @lc code=start
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        dp = [[0 for j in range(n+1)] for i in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        # print(dp)
+        return dp[m][n]
+
+
+# @lc code=end
+r = Solution().longestCommonSubsequence('abcde', 'ace')
+assert r == 3
+
+r = Solution().longestCommonSubsequence('abc', 'def')
+assert r == 0
