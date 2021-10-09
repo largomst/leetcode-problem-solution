a8ddfbe22e7e865fe36b900d0b6f2ecd94cc011f
largomst <dingrui.zhen@gmail.com>
2021-10-09 18:28:20 +0800

    complement comment

--- 1143.最长公共子序列.2.py
+++ 1143.最长公共子序列.2.py
@@ -4,19 +4,24 @@
 # [1143] 最长公共子序列
 #
 
+from pprint import pprint
 # @lc code=start
+
+
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         m = len(text1)
         n = len(text2)
-        dp = [[0 for j in range(n+1)] for i in range(m+1)]
+        # base case
+        dp = [[0 for j in range(n+1)]
+              for i in range(m+1)]  # 考虑边界情况，将 dp 数组扩大一点
         for i in range(1, m+1):
             for j in range(1, n+1):
-                if text1[i-1] == text2[j-1]:
+                if text1[i-1] == text2[j-1]:  # 适应扩大的 dp 产生的偏移
                     dp[i][j] = 1 + dp[i-1][j-1]
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        # print(dp)
+        pprint(dp, width=15)
         return dp[m][n]
 
 
