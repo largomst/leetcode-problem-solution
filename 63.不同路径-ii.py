b9f180c05582bceb87410d78977a41156c0522da
largomst <dingrui.zhen@gmail.com>
2021-10-11 17:37:48 +0800

    add 63

---
+++ 63.不同路径-ii.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode.cn id=63 lang=python3
+#
+# [63] 不同路径 II
+#
+
+# @lc code=start
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        if m == 0 or n == 0:
+            return 0
+        dp = [[0 for j in range(n)] for i in range(m)]
+        for i in range(m):
+            if obstacleGrid[i][0] == 0:
+                dp[i][0] = 1
+            else:
+                break
+        for j in range(n):
+            if obstacleGrid[0][j] == 0:
+                dp[0][j] = 1
+            else:
+                break
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 1:
+                    continue
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
+
+# @lc code=end
