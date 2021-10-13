f0dcb548716c0c838603128adaeb541b8c2c5e29
largomst <dingrui.zhen@gmail.com>
2021-10-13 22:13:15 +0800

    add 1049

---
+++ 1049.最后一块石头的重量-ii.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=1049 lang=python3
+#
+# [1049] 最后一块石头的重量 II
+#
+
+# @lc code=start
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        n = len(stones)
+        amount = sum(stones)
+        target = amount // 2
+        dp = [0] * (target+1)
+        for i in range(0, n):
+            for j in range(target, stones[i]-1, -1):
+                dp[j] = max(dp[j], dp[j-stones[i]]+stones[i])
+        return (amount - dp[target]) - dp[target]
+
+
+# @lc code=end
