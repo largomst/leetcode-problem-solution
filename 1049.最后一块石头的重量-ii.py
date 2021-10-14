57687b305a12887867b9e8c325ce4c31141f6109
largomst <dingrui.zhen@gmail.com>
2021-10-14 15:50:13 +0800

    add 1049

--- 1049.最后一块石头的重量-ii.py
+++ 1049.最后一块石头的重量-ii.py
@@ -10,7 +10,8 @@ class Solution:
         n = len(stones)
         amount = sum(stones)
         target = amount // 2
-        dp = [0] * (target+1)
+        dp = [0] * (target+1)  # dp[j] 表示容量为 j 的背包能够容纳的石头重量的最大值
+        dp[0] = 0  # base case
         for i in range(0, n):
             for j in range(target, stones[i]-1, -1):
                 dp[j] = max(dp[j], dp[j-stones[i]]+stones[i])
